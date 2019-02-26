8e8448d2567d6201ab0f80d1543549dd313e4ecb
Vicksiy <48007782+Vicksiy@users.noreply.github.com>
2019-02-26 15:29:56 +0800

    Add files via upload

---
+++ autoload.js
@@ -0,0 +1,26 @@
+const live2d_path = "https://cdn.jsdelivr.net/gh/Devilcky/live2dwt/";
+
+$("<link>").attr({href: live2d_path + "waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
+//waifu.css的绝对路径
+
+$.ajax({
+	url: live2d_path + "live2d.min.js",
+	dataType: "script",
+	cache: true,
+	async: false
+});
+//live2d.min.js的绝对路径
+
+$.ajax({
+	url: live2d_path + "waifu-tips.js",
+	dataType: "script",
+	cache: true,
+	async: false
+});
+//waifu-tips.js的绝对路径
+
+
+$(window).on("load", function() {
+	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
+});
+
